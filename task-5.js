a57bf3c84ea69bfa6d7ffce83cce0752aaa4cbd0
Victor <you@example.com>
2021-01-03 23:40:00 +0200

    исправил ask-5

--- task-5.js
+++ task-5.js
@@ -1,35 +1,31 @@
 let countryName = prompt("Введите страну доставки товара", "");
 countryName =
-  countryName.charAt(0).toUpperCase() + countryName.slice(1).toLowerCase(); // приводим название введенной страны к нормальному виду Xxxxx
+  countryName.charAt(0).toUpperCase() + countryName.slice(1).toLowerCase(); 
+  // приводим название введенной страны к нормальному виду Xxxxx
 //console.log(countryName);
 let names = {
-  name1: "Китай",
-  name2: "Чили",
-  name3: "Австралия",
-  name4: "Индия",
-  name5: "Ямайка",
+    "Китай": 150,
+    "Чили": 250,
+    "Австралия":165,
+    "Индия": 90,
+    "Ямайка":130,
 };
-let prices = { 
-  price1: 150, 
-  price2: 250, 
-  price3: 165, 
-  price4: 90, 
-  price: 130 };
+
 switch (countryName) {
   case "Китай":
-    alert(`Доставка в ${names.name1} будет стоить ${prices.price1} кредитов`);
+    alert(`Доставка в ${countryName} будет стоить ${names["Китай"]} кредитов`);
     break;
   case "Чили":
-    alert(`Доставка в ${names.name2} будет стоить ${prices.price2} кредитов`);
+    alert(`Доставка в ${countryName} будет стоить ${names["Чили"]} кредитов`);
     break;
   case "Австралия":
-    alert(`Доставка в ${names.name3} будет стоить ${prices.price3} кредитов`);
+    alert(`Доставка в ${countryName} будет стоить ${names["Австралия"]} кредитов`);
     break;
   case "Индия":
-    alert(`Доставка в ${names.name4} будет стоить ${prices.price4} кредитов`);
+    alert(`Доставка в ${countryName} будет стоить ${names["Индия"]} кредитов`);
     break;
   case "Ямайка":
-    alert(`Доставка в ${names.name5} будет стоить ${prices.price5} кредитов`);
+    alert(`Доставка в ${countryName} будет стоить ${names["Ямайка"]} кредитов`);
     break;
   default:
     alert("В вашей стране доставка не доступна");
